b63305a1f42c1bf5e35d231939558ee83c96bf9a
Marcos Oliveira <marcos-oliveirasilva@hotmail.com>
2021-04-29 21:07:10 -0300

    0.0.1

---
+++ app.js
@@ -0,0 +1,15 @@
+
+function ligar(){
+  var lampada = document.getElementById('lamp')
+  lampada.src = "./img/ligada.jpg"
+  
+}
+
+function quebra(){
+  var lampada = document.getElementById('lamp')
+  lampada.src = "./img/quebrada.jpg"
+}
+function desliga(){
+  var lampada = document.getElementById('lamp')
+  lampada.src ="./img/desligada.jpg"
+}
